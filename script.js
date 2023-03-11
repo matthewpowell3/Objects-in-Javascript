4aebff5b3e4233d775e196480eea3cca817ebf21
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-11 14:26:42 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Objects in Javascript
 
-var car_maker = 'MarkX';
-var car_model = 'X1';
-var price = 50000;
+//objects are bondle
+var car = {
+  car_maker: 'MarkX',
+  car_model: 'X1',
+  price: 50000,
+};
+
+// var car_maker = 'MarkX';
+// var car_model = 'X1';
+// var price = 50000;
+
+// function allcar() {
+//   document.write('THis car is with nice car');
+// }
+
+// allcar();
